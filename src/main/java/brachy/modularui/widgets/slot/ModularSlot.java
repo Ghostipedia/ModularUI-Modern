1b690d29a3fd5c0284943b9b7fcd46b2e2ed06ae
brachy84 <brachy84@gmail.com>
2026-03-17 18:11:51 +0100

    port ea66aba7, skip fbd62451

--- src/main/java/brachy/modularui/widgets/slot/ModularSlot.java
+++ src/main/java/brachy/modularui/widgets/slot/ModularSlot.java
@@ -1,5 +1,6 @@
 package brachy.modularui.widgets.slot;
 
+import brachy.modularui.core.mixins.client.CombinedInvWrapperAccessor;
 import brachy.modularui.value.sync.ItemSlotSyncHandler;
 
 import net.minecraft.resources.ResourceLocation;
@@ -9,7 +10,9 @@ import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import com.mojang.datafixers.util.Pair;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.SlotItemHandler;
+import net.minecraftforge.items.wrapper.PlayerArmorInvWrapper;
 import net.minecraftforge.items.wrapper.PlayerInvWrapper;
 import net.minecraftforge.items.wrapper.PlayerMainInvWrapper;
 
@@ -262,5 +265,27 @@ public class ModularSlot extends SlotItemHandler {
     public static boolean isPlayerSlot(SlotItemHandler slot) {
         return slot.getItemHandler() instanceof PlayerInvWrapper ||
                 slot.getItemHandler() instanceof PlayerMainInvWrapper;
+    }
+
+    public static Player getPlayerSlotPlayer(Slot slot) {
+        return slot.container instanceof Inventory inv ? inv.player : null;
+    }
+
+    public static Player getPlayerSlotPlayer(SlotItemHandler slot) {
+        if (slot.getItemHandler() instanceof PlayerInvWrapper inv) {
+            for (IItemHandlerModifiable ih : ((CombinedInvWrapperAccessor) inv).getItemHandler()) {
+                if (ih instanceof PlayerMainInvWrapper mainInv) {
+                    return mainInv.getInventoryPlayer().player;
+                }
+            }
+            return null;
+        }
+        if (slot.getItemHandler() instanceof PlayerMainInvWrapper wrapper) {
+            return wrapper.getInventoryPlayer().player;
+        }
+        if (slot.getItemHandler() instanceof PlayerArmorInvWrapper wrapper) {
+            return wrapper.getInventoryPlayer().player;
+        }
+        return null;
     }
 }
